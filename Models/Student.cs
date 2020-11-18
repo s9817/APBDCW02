3d830d6916aac64793561c3e7fd67095b2ab9ed7
s9817 <s9817@pjwstk.edu.pl>
2020-11-18 23:30:01 +0100

    Cwiczenie 02

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APBD_CW02.Models
+{
+    class Student
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APBD_CW02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
